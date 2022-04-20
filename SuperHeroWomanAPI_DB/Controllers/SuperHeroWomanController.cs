8983c2b63d7c71ac344afe47a113d0d9b8ec3d26
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-20 19:58:44 -0300

    Adicionar arquivos de projeto.

---
+++ SuperHeroWomanAPI_DB/Controllers/SuperHeroWomanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroWomanAPI_DB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroWomanController : ControllerBase
+    {
+
+    }
+}
---
+++ SuperHeroWomanAPI_DB/SuperHeroWoman.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroWomanAPI_DB
+{
+    public class SuperHeroWoman
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public string Actions { get; set; } = string.Empty;
+        public string LocalActions { get; set; } = string.Empty;
+    }
+}
