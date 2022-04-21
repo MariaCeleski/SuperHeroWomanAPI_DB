d68e8cdd1f745fe3bd2c8f41d7e9668cc79c3c7c
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-20 23:02:54 -0300

    Banco de dados

--- SuperHeroWomanAPI_DB/Data/DataContext.cs
+++ SuperHeroWomanAPI_DB/Data/DataContext.cs
@@ -4,6 +4,8 @@ namespace SuperHeroWomanAPI_DB.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext>options) : base(options) { }
 
+        public DbSet<SuperHeroWoman> SuperHeroes { get; set; }
     }
 }
