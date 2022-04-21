4d5a056600338e6d5672eeaf43eb4e94c80dc1bb
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-21 20:37:02 -0300

    Finalizando integração com DB

--- SuperHeroWomanAPI_DB/Controllers/SuperHeroWomanController.cs
+++ SuperHeroWomanAPI_DB/Controllers/SuperHeroWomanController.cs
@@ -8,49 +8,49 @@ namespace SuperHeroWomanAPI_DB.Controllers
     public class SuperHeroWomanController : ControllerBase
     {
          
-        private static List<SuperHeroWoman> heroes = new List<SuperHeroWoman>
-            {
-                new SuperHeroWoman{
-                    Id = 1,
-                    Name = "Django Girls",
-                    FirstName = "Ola",
-                    LastName = "SitarskaSendecka",
-                    Place = "Poland",
-                    Actions = "Investment",
-                    LocalActions = "Savings"
-                },
+        //private static List<SuperHeroWoman> heroes = new List<SuperHeroWoman>
+        //    {
+        //        new SuperHeroWoman{
+        //            Id = 1,
+        //            Name = "Django Girls",
+        //            FirstName = "Ola",
+        //            LastName = "SitarskaSendecka",
+        //            Place = "Poland",
+        //            Actions = "Investment",
+        //            LocalActions = "Savings"
+        //        },
 
-                new SuperHeroWoman{
-                    Id = 2,
-                    Name = "Programa Maria",
-                    FirstName = "Empoderando",
-                    LastName = "MeninasEMulheres",
-                    Place = "Brazil",
-                    Actions = "Investment",
-                    LocalActions = "Stock Exchange"
-                },
+        //        new SuperHeroWoman{
+        //            Id = 2,
+        //            Name = "Programa Maria",
+        //            FirstName = "Empoderando",
+        //            LastName = "MeninasEMulheres",
+        //            Place = "Brazil",
+        //            Actions = "Investment",
+        //            LocalActions = "Stock Exchange"
+        //        },
 
-                new SuperHeroWoman{
-                    Id = 3,
-                    Name = "GRACE",
-                    FirstName = "Kalinka Branco",
-                    LastName = "Maria Lydya Fioravanti",
-                    Place = "Brazil",
-                    Actions = "Investment",
-                    LocalActions = "Savings"
-                },
+        //        new SuperHeroWoman{
+        //            Id = 3,
+        //            Name = "GRACE",
+        //            FirstName = "Kalinka Branco",
+        //            LastName = "Maria Lydya Fioravanti",
+        //            Place = "Brazil",
+        //            Actions = "Investment",
+        //            LocalActions = "Savings"
+        //        },
 
-                new SuperHeroWoman{
-                    Id = 4,
-                    Name = "WoMakersCode",
-                    FirstName = "Cynthia",
-                    LastName = "Zanoni",
-                    Place = "Brazil",
-                    Actions = "Investment",
-                    LocalActions = "Stock Exchange"
-                }
+        //        new SuperHeroWoman{
+        //            Id = 4,
+        //            Name = "WoMakersCode",
+        //            FirstName = "Cynthia",
+        //            LastName = "Zanoni",
+        //            Place = "Brazil",
+        //            Actions = "Investment",
+        //            LocalActions = "Stock Exchange"
+        //        }
 
-            };
+        //    };
         private readonly DataContext _context;
 
         public SuperHeroWomanController(DataContext context)
@@ -87,29 +87,33 @@ namespace SuperHeroWomanAPI_DB.Controllers
         [HttpPut]
         public async Task<ActionResult<List<SuperHeroWoman>>> UpdateHero(SuperHeroWoman request)
         {
-            var hero = heroes.Find(h => h.Id == request.Id);
-            if (hero == null)
+            var dbHero = await _context.SuperHeroes.FindAsync(request.Id);
+            if (dbHero == null)
                 return BadRequest("Hero not found.");
 
-            hero.Name = request.Name;
-            hero.FirstName = request.FirstName;
-            hero.LastName = request.LastName;
-            hero.Place = request.Place;
-            hero.Actions = request.Actions;
-            hero.LocalActions = request.LocalActions;
+            dbHero.Name = request.Name;
+            dbHero.FirstName = request.FirstName;
+            dbHero.LastName = request.LastName;
+            dbHero.Place = request.Place;
+            dbHero.Actions = request.Actions;
+            dbHero.LocalActions = request.LocalActions;
 
-            return Ok(heroes);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<SuperHeroWoman>>> Delete(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
-            if (hero == null)
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+            if (dbHero == null)
                 return BadRequest("Hero not found.");
 
-            heroes.Remove(hero);
-            return Ok(heroes);
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
     }
